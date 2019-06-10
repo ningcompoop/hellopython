061eb57cd08dc0d5e03bcd98c857a2f80b5d96eb
ningcompoop <47544347+ningcompoop@users.noreply.github.com>
2019-06-10 16:51:18 -0700

    Exercise 2
    Factorial program

---
+++ scratch.py
@@ -0,0 +1,12 @@
+#factorial calculation
+#need to have someone input number
+
+import math #can use math.factorial
+#print(math.factorial(5))
+y = input("Define y:")
+y=int(y)
+print(math.factorial(y))
+
+
+
+
