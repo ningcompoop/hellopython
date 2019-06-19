41444154a8137eea1536537a249730202b7589c2
ningcompoop <47544347+ningcompoop@users.noreply.github.com>
2019-06-19 16:48:07 -0700

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,11 +1,24 @@
-#factorial calculation
-#need to have someone input number
+#Exercise 5
+#define class which as at least 2 methods:
+#getString, printString
 
-import math #can use math.factorial
-#print(math.factorial(5))
-y = input("Define y:")
-y=int(y)
-print(math.factorial(y))
+
+class InputOutString(object):
+    def _init__(self):
+       self.x = ""
+
+    def getString(self):
+        self.x = input("Provide a list of school subjects: ")
+
+    def printString(self):
+        print(self.x.upper())
+
+strObj = InputOutString()
+strObj.getString()
+#looks for the user input from def getString(self)
+#another way to look for input instead of x=input("User input")
+strObj.printString()
+#prints the input out with all caps from the def printString(self)
 
 
 
